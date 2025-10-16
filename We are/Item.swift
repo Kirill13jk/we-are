04c714e3ed081eb920b91c9dc7a65600937cbfb2
Kirill <onlyjesus13@icloud.com>
2025-10-16 11:24:25 +0500

    Initial Commit

---
+++ We are/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  We are
+//
+//  Created by MacBook on 16/10/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ We areTests/We_areTests.swift
@@ -0,0 +1,17 @@
+//
+//  We_areTests.swift
+//  We areTests
+//
+//  Created by MacBook on 16/10/25.
+//
+
+import Testing
+@testable import We_are
+
+struct We_areTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
